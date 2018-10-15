4ff3dcb9b7a4b6270d6069931e3daa98edc484e8
TrOsKa11 <zem35157@spseol.cz>
2018-10-15 10:06:44 +0200

    Přidáno ABC

--- debugapp.py
+++ debugapp.py
@@ -32,6 +32,9 @@ def index():
 def onas():
     return render_template('onas.html')
 
+@app.route('/ABC/')
+def ABC():
+    return render_template('ABC.html')
 
 ############################################################################
 if __name__ == '__main__':
